aeae6af3912c8a5c766d0727feb28acd496fcddc
Athitheya Gobinathan <athith.g@gmail.com>
2024-05-30 14:58:02 -0400

    docs: add docstrings

--- tests/tasks/test_tasks.py
+++ tests/tasks/test_tasks.py
@@ -1,3 +1,5 @@
+"""Tests for I/O and decryption tasks"""
+
 from time import sleep
 import json
 import requests
@@ -17,18 +19,21 @@ input_text = "hello world from the input!"
 
 
 def create_task(tasks_body):
+    """Creates task with the given task body."""
     return requests.post(
         url=f"{tes_url}/tasks", headers=headers, json=tasks_body
     )
 
 
 def get_task(task_id):
+    """Retrieves list of tasks."""
     return requests.get(
         url=f"{tes_url}/tasks/{task_id}", headers=headers
     )
 
 
 def get_task_state(task_id):
+    """Retrieves state of task until completion."""
     def wait_for_task_completion():
         nonlocal task_state
         get_response = get_task(task_id)
@@ -49,11 +54,13 @@ def get_task_state(task_id):
     uppercase_task_with_decryption_body
 ])
 def post_response(request):
+    """Returns response received after creating task."""
     return create_task(request.param)
 
 
 @pytest.fixture
 def task_state(post_response):
+    """Returns state of task after completion."""
     task_id = json.loads(post_response.text)["id"]
     return get_task_state(task_id)
 
@@ -64,6 +71,7 @@ def task_state(post_response):
     ("hello-upper-decrypt.txt", input_text.upper())
 ])
 def test_task(post_response, task_state, filename, expected_output):
+    """Test tasks for successful completion and intended behavior."""
     assert post_response.status_code == 200
     assert task_state == "COMPLETE"
 
