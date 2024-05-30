6831a0f0862810506d4b658c1e91c4cc085d2f27
athith-g <66563785+athith-g@users.noreply.github.com>
2024-05-30 12:57:35 -0400

    test: add input files (#8)

---
+++ inputs/make_uppercase.py
@@ -0,0 +1,12 @@
+import sys
+
+
+def get_uppercase_contents(file_name):
+    with open(file_name, encoding='utf-8') as f:
+        content = f.read()
+        return content.upper()
+
+
+if __name__ == '__main__':
+    file = sys.argv[1]
+    print(get_uppercase_contents(file), end='')
