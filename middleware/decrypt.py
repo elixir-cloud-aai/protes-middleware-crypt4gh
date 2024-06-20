be41af06fd6045eda8b1c51e7e0e453f1461ac7a
Athitheya Gobinathan <athith.g@gmail.com>
2024-06-20 18:42:59 -0400

    refactor: create new functions

--- middleware/decrypt.py
+++ middleware/decrypt.py
@@ -1,9 +1,8 @@
 """Identify and decrypt Crypt4GH keys and files.
 
-Decrypts crypt4GH files given a list of files and places the output in a specified
-directory. If a file is not Crypt4GH-encrypted, it moves the file to the output directory
-without alteration. If a Crypt4GH file is included, the private key associated with that
-file must be provided.
+Moves all files in a given list and places the output in a specified directory. Any encrypted files
+are subsequently decrypted in place. If a Crypt4GH file is included, the private key associated with
+that file must be provided.
 
 Example:
     python3 decrypt.py --output-dir /outputs/ file.txt file.c4gh sk.sec pk.pub
@@ -30,16 +29,14 @@ def get_private_keys(file_paths: list[Path]) -> list[bytes]:
     for file_path in file_paths:
         try:
             # Callback returns password of sk
-            key = get_private_key(file_path, callback=lambda x: '')
+            key = get_private_key(filepath=file_path, callback=lambda x: '')
             private_keys.append(key)
         except ValueError:
             continue
     return private_keys
 
 
-def decrypt_files(
-        file_paths: list[Path],
-        private_keys: list[bytes]):
+def decrypt_files(file_paths: list[Path], private_keys: list[bytes]):
     """Decrypt files in place.
 
     Args:
@@ -66,19 +63,41 @@ def decrypt_files(
                 raise ValueError(f"Private key for {file_path} not provided") from e
 
 
-if __name__ == "__main__":
+def move_files(file_paths: list[Path], output_dir: Path) -> list[Path]:
+    """Decrypt files in place.
+
+        Args:
+            file_paths: A list of file paths.
+            output_dir: Directory to move files to.
+
+        Returns:
+            A list of the new file paths.
+        """
+    output_paths = [output_dir/file_path.name for file_path in file_paths]
+    for src, dest in zip(file_paths, output_paths):
+        shutil.move(src, dest)
+    return output_paths
+
+
+def main():
+    """Parse command-line arguments"""
     parser = ArgumentParser()
-    parser.add_argument("file_paths", nargs='+', type=Path)
+    parser.add_argument(
+        "file_paths",
+        nargs='+',
+        type=Path,
+        help="Paths to input files.")
     parser.add_argument(
         "--output-dir",
         required=True,
-        dest="output_dir",
         help="Directory to upload files to.",
         type=Path)
 
     args = parser.parse_args()
-    paths = [args.output_dir/file_path.name for file_path in args.file_paths]
-    for src, dest in zip(args.file_paths, paths):
-        shutil.move(src, dest)
-    keys = get_private_keys(paths)
-    decrypt_files(paths, keys)
+    new_paths = move_files(file_paths=args.file_paths, output_dir=args.output_dir)
+    keys = get_private_keys(new_paths)
+    decrypt_files(file_paths=new_paths, private_keys=keys)
+
+
+if __name__ == "__main__":
+    main()
