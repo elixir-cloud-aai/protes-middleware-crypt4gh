224de36f92f6639dbde9ab6893a1e65fb300b29b
Athitheya Gobinathan <athith.g@gmail.com>
2024-08-05 15:28:41 -0400

    test: test decryption of files

--- tests/decryption/test_integration.py
+++ tests/decryption/test_integration.py
@@ -1,15 +1,22 @@
 """Integration tests for decrypt.py."""
 
 import pytest
+
+from crypt4gh_middleware.decrypt import main
 from tests.utils import patch_cli, INPUT_TEXT, INPUT_DIR
 
-@pytest.mark.parametrize("files", [
-    ["alice.sec", "hello.c4gh"],
-    ["alice.sec", "bob.sec", "hello.c4gh", "hello2.c4gh"]
-])
-def test_decryption(files):
-    """Test that files can be decrypted successfully."""
 
+@pytest.mark.parametrize("keys,files", [
+    [["alice.sec"], ["hello.c4gh"]],
+    [["alice.sec", "bob.sec"], ["hello.c4gh", "hello2.c4gh"]]
+])
+def test_decryption(keys, files, tmp_path):
+    """Test that files can be decrypted successfully."""
+    patch_cli(["--output-dir", tmp_path] + [INPUT_DIR/f for f in files] + keys)
+    main()
+    for filename in files:
+        with open(tmp_path/filename, encoding="utf-8") as f:
+            assert f.read() == INPUT_TEXT
 
 def test_default_dir():
     """Test that $TMPDIR is used when no output dir is provided."""
