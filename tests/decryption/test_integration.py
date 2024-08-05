113c70ab9d1da6d91ed9e876c3f60202e0065e58
Athitheya Gobinathan <athith.g@gmail.com>
2024-08-05 17:14:34 -0400

    test: complete test_decryption

--- tests/decryption/test_integration.py
+++ tests/decryption/test_integration.py
@@ -9,16 +9,18 @@ from tests.utils import patch_cli, INPUT_TEXT, INPUT_DIR
 
 @pytest.fixture(name="secret_keys")
 def fixture_secret_keys(tmp_path):
-    keys = ["alice.sec", "bob.sec"]
+    """Returns temporary copies of encrypted files."""
+    encrypted_files = [INPUT_DIR/"alice.sec", INPUT_DIR/"bob.sec"]
+    temp_files = [tmp_path/"alice.sec", tmp_path/"bob.sec"]
+    for src, dest in zip(encrypted_files, temp_files):
+        shutil.copy(src, dest)
+    return temp_files
 
 
-@pytest.mark.parametrize("keys", [
-    [["alice.sec"], ["hello.c4gh"]]
-])
-def test_decryption(keys, encrypted_files, tmp_path):
+def test_decryption(secret_keys, encrypted_files, tmp_path):
     """Test that files can be decrypted successfully."""
-    with patch_cli(["decrypt.py", "--output-dir", tmp_path.as_posix()]
-                   + [f for f in encrypted_files] + keys):
+    with patch_cli(["decrypt.py", "--output-dir", str(tmp_path)]
+                   + [str(f) for f in (encrypted_files + secret_keys)]):
         main()
         for filename in encrypted_files:
             with open(tmp_path/filename, encoding="utf-8") as f:
