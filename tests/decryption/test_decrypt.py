6167df52117fd5e10699860f8a299be88600e39d
Athitheya Gobinathan <athith.g@gmail.com>
2024-08-06 13:43:42 -0400

    revert: undo removal of local encrypted_files fixture

--- tests/decryption/test_decrypt.py
+++ tests/decryption/test_decrypt.py
@@ -64,6 +64,15 @@ class TestDecryptFiles:
         pk_bytes = get_pk_bytes(filepath=pk)
         return sk_bytes, pk_bytes
 
+    @pytest.fixture(name="encrypted_files")
+    def fixture_encrypted_files(self, tmp_path):
+        """Returns temporary copies of encrypted files."""
+        encrypted_files = [INPUT_DIR / "hello.c4gh", INPUT_DIR / "hello2.c4gh"]
+        temp_files = [tmp_path / "hello.c4gh", tmp_path / "hello2.c4gh"]
+        for src, dest in zip(encrypted_files, temp_files):
+            shutil.copy(src, dest)
+        return temp_files
+
     @pytest.fixture(name="unencrypted_files")
     def fixture_unencrypted_files(self):
         """Returns the unencrypted file paths"""
