73e9d19a096e28dd22d1f261b498525a6b454ee7
Athitheya Gobinathan <athith.g@gmail.com>
2024-08-06 13:22:38 -0400

    docs: update docstring for encrypted files

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,7 +9,9 @@ from tests.utils import INPUT_DIR
 
 @pytest.fixture(name="encrypted_files")
 def fixture_encrypted_files(tmp_path):
-    """Returns temporary copies of encrypted files."""
+    """Returns temporary copies of encrypted files.
+    First two files are encrypted with alice.sec. Third file is encrypted with bob.sec.
+    """
     encrypted_files = [INPUT_DIR/"hello.c4gh", INPUT_DIR/"hello2.c4gh", INPUT_DIR/"hello3.c4gh"]
     temp_files = [tmp_path/f.name for f in encrypted_files]
     for src, dest in zip(encrypted_files, temp_files):
