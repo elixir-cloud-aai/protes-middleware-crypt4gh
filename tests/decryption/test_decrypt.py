dd967e986f25071f8e1e10c8bb1c55b7b7c8d5b2
Athitheya Gobinathan <athith.g@gmail.com>
2024-08-06 13:45:04 -0400

    refactor: remove whitespace

--- tests/decryption/test_decrypt.py
+++ tests/decryption/test_decrypt.py
@@ -67,8 +67,8 @@ class TestDecryptFiles:
     @pytest.fixture(name="encrypted_files")
     def fixture_encrypted_files(self, tmp_path):
         """Returns temporary copies of encrypted files."""
-        encrypted_files = [INPUT_DIR / "hello.c4gh", INPUT_DIR / "hello2.c4gh"]
-        temp_files = [tmp_path / "hello.c4gh", tmp_path / "hello2.c4gh"]
+        encrypted_files = [INPUT_DIR/"hello.c4gh", INPUT_DIR/"hello2.c4gh"]
+        temp_files = [tmp_path/"hello.c4gh", tmp_path/"hello2.c4gh"]
         for src, dest in zip(encrypted_files, temp_files):
             shutil.copy(src, dest)
         return temp_files
