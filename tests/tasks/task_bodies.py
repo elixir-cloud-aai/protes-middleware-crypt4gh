a3d927424d48e8cc2734db143f8e5f4d813baf31
Athitheya Gobinathan <athith.g@gmail.com>
2024-09-01 15:28:40 -0400

    feat: change input and output to minio buckets

--- tests/tasks/task_bodies.py
+++ tests/tasks/task_bodies.py
@@ -1,26 +1,26 @@
 """TES task request bodies used by tests"""
 
-from tests.utils import INPUT_DIR
+from tests.utils import S3_BUCKET_ENDPOINT
 
-def get_uppercase_task_body(tmp_dir):
+def get_uppercase_task_body(output_bucket_name):
     """Returns TES task body that makes the contents of a file uppercase."""
     return {
         "name": "Hello world",
         "inputs": [
             {
-                "url": f"file://{INPUT_DIR}/hello.txt",
+                "url": f"s3://inputs/hello.txt",
                 "path": "/inputs/hello.txt",
                 "type": "FILE"
             },
             {
-                "url": f"file://{INPUT_DIR}/make_uppercase.py",
+                "url": f"s3://inputs/make_uppercase.py",
                 "path": "/inputs/make_uppercase.py",
                 "type": "FILE"
             }
         ],
         "outputs": [
             {
-                "url": f"file://{tmp_dir}/hello-upper.txt",
+                "url": f"s3://{output_bucket_name}/hello-upper.txt",
                 "path": "/outputs/hello-upper.txt",
                 "type": "FILE"
             }
@@ -39,30 +39,30 @@ def get_uppercase_task_body(tmp_dir):
     }
 
 
-def get_decryption_task_body(tmp_dir):
+def get_decryption_task_body(output_bucket_name):
     """Returns TES task body that decrypts a crypt4GH file."""
     return {
         "name": "Decrypt with secret key as environment variable",
         "inputs": [
             {
-                "url": f"file://{INPUT_DIR}/hello.c4gh",
+                "url": f"http://{S3_BUCKET_ENDPOINT}/inputs/hello.c4gh",
                 "path": "/inputs/hello.c4gh",
                 "type": "FILE"
             },
             {
-                "url": f"file://{INPUT_DIR}/alice.sec",
+                "url": f"http://{S3_BUCKET_ENDPOINT}/inputs/alice.sec",
                 "path": "/inputs/alice.sec",
                 "type": "FILE"
             },
             {
-                "url": f"file://{INPUT_DIR}/decrypt.sh",
+                "url": f"http://{S3_BUCKET_ENDPOINT}/inputs/decrypt.sh",
                 "path": "/inputs/decrypt.sh",
                 "type": "FILE"
             }
         ],
         "outputs": [
             {
-                "url": f"file://{tmp_dir}/hello-decrypted.txt",
+                "url": f"s3://{output_bucket_name}/hello-decrypted.txt",
                 "path": "/outputs/hello-decrypted.txt",
                 "type": "FILE"
             }
@@ -81,35 +81,35 @@ def get_decryption_task_body(tmp_dir):
     }
 
 
-def get_uppercase_task_with_decryption_body(tmp_dir):
+def get_uppercase_task_with_decryption_body(output_bucket_name):
     """Returns TES task body that makes the contents of a crypt4GH file uppercase."""
     return {
         "name": "Decrypt with secret key as environment variable",
         "inputs": [
             {
-                "url": f"file://{INPUT_DIR}/hello.c4gh",
+                "url": f"http://{S3_BUCKET_ENDPOINT}/inputs/hello.c4gh",
                 "path": "/inputs/hello.c4gh",
                 "type": "FILE"
             },
             {
-                "url": f"file://{INPUT_DIR}/alice.sec",
+                "url": f"http://{S3_BUCKET_ENDPOINT}/inputs/alice.sec",
                 "path": "/inputs/alice.sec",
                 "type": "FILE"
             },
             {
-                "url": f"file://{INPUT_DIR}/decrypt.sh",
+                "url": f"http://{S3_BUCKET_ENDPOINT}/inputs/decrypt.sh",
                 "path": "/inputs/decrypt.sh",
                 "type": "FILE"
             },
             {
-                "url": f"file://{INPUT_DIR}/make_uppercase.py",
+                "url": f"http://{S3_BUCKET_ENDPOINT}/inputs/make_uppercase.py",
                 "path": "/inputs/make_uppercase.py",
                 "type": "FILE"
             }
         ],
         "outputs": [
             {
-                "url": f"file://{tmp_dir}/hello-upper-decrypt.txt",
+                "url": f"s3://{output_bucket_name}/hello-upper-decrypt.txt",
                 "path": "/outputs/hello-upper-decrypt.txt",
                 "type": "FILE"
             }
